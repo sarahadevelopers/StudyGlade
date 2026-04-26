b81c2fa995b71440d92b6a47b2394cb9a45430b7
Charles Karanja <codewithkaranja@gmail.com>
2026-04-26 09:07:46 +0300

    Add global error handler for debugging

--- server.js
+++ server.js
@@ -100,4 +100,8 @@ cron.schedule('0 * * * *', async () => {
 // -----------------------------------------------------------------
 
 const PORT = process.env.PORT || 5000;
+app.use((err, req, res, next) => {
+  console.error('Global error:', err.stack);
+  res.status(500).json({ error: err.message });
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
